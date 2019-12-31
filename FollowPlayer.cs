7268ef850bd4f65c0bb1deb6067f2911a868412e
preetmodh <54811605+preetmodh@users.noreply.github.com>
2019-12-31 19:15:32 +0530

    Update FollowPlayer.cs

--- FollowPlayer.cs
+++ FollowPlayer.cs
@@ -2,10 +2,10 @@
 
 public class FollowPlayer : MonoBehaviour {
 
-	public Transform player;	// A variable that stores a reference to our Player
-	public Vector3 offset;		// A variable that allows us to offset the position (x, y, z)
+	public Transform player;	
+	public Vector3 offset;		
 	
-	// Update is called once per frame
+	
 	void Update () {
 		// Set our position to the players position and offset it
 		transform.position = player.position + offset;
